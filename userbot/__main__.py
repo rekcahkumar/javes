e8449a1cc1b1fe7f6b7949aecaa4ebe93a99cc83
rekcahkumar <48340011+rekcahkumar@users.noreply.github.com>
2020-08-06 18:33:22 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,4 +1,4 @@
-from userbot import * ;  from sys import * ; from telethon import TelegramClient, functions, types ; from telethon.tl.types import InputMessagesFilterDocument ; from pathlib import Path; from userbot.javes_main.commands import * ; import asyncio, os, traceback, sys, traceback, os, importlib, glob ; javes = tgbot = client 
+from userbot import * ;  from sys import * ; from telethon import TelegramClient, functions, types ; from telethon.tl.types import InputMessagesFilterDocument ; from pathlib import Path; from userbot.javes_main.commands import * ; import asyncio, os, traceback, sys, traceback, os, importlib, glob ; javes = bot.tgbot = tgbot = client 
 from telethon.tl.types import InputMessagesFilterDocument
 from importlib import import_module
 
