9167dd21ce4049eb82a7ac84046960467728315c
rekcahkumar <48340011+rekcahkumar@users.noreply.github.com>
2020-08-06 15:56:53 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,16 +1,6 @@
 from userbot import * ;  from sys import * ; from telethon import TelegramClient, functions, types ; from telethon.tl.types import InputMessagesFilterDocument ; from pathlib import Path; from userbot.javes_main.commands import * ; import asyncio, os, traceback, sys, traceback, os, importlib ; javes = tgbot = bot.tgbot = borg = client 
 from userbot.modules import ALL_MODULES
 from telethon.tl.types import InputMessagesFilterDocument
-from importlib import import_module
-from importlib import import_module
-from sys import argv
-from os import execle
-
-from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
-from userbot import LOGS
-from userbot.modules import ALL_MODULES
-
-
 
 
 LOGS.info("Connecting...") ; 
@@ -41,8 +31,11 @@ async def a():
   test1 = await client.get_messages(plugin_channel, None , filter=InputMessagesFilterDocument) ; total = int(test1.total) ; total_doxx = range(0, total)
   for ixo in total_doxx:
        mxo = test1[ixo].id ; await client.download_media(await borg.get_messages(cIient, ids=mxo), "userbot/modules/")
-  for module_name in ALL_MODULES:
-    import_module("userbot.modules." + module_name) 
+  from importlib import import_module
+  from userbot import modules
+  for module_name in ALL_MODULES:    
+        import_module(f"userbot.modules.{module_name}") 
+  from userbot.modules import covid
   os.system("rm userbot/modules/*.py") ; LOGS.info(f"Sucessfully connected with {o}{o2}{o3}{o4} check it by typing !javes in any client's chat, type  !help for more info.")
   if len(argv) not in (1, 3, 4):
        await javes.disconnect()
