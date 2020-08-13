7c29f9d2b9e8d15525459e598e0c6d5eec9f7484
rekcahkumar <48340011+rekcahkumar@users.noreply.github.com>
2020-08-13 21:23:12 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -15,7 +15,7 @@ async def a():
   o = o2 = o3 = o4 = ""
   la = 0
   try:
-     await client.start() ; LOGS.info("client connected") ; o = "Client1" : OWNER.update({await client.get_me()})
+     await client.start() ; LOGS.info("client connected") ; o = "Client1" ; me = await client.get_me.() ; OWNER.update({me.id})
   except:
     LOGS.info("Telegram String Session Wrong or Expired Please Add new one ") ; quit(1)
   if client2:
