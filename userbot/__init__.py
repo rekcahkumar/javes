4f8da3f5d1748abd6e165995c800aadbd0c8d08c
rekcahkumar <48340011+rekcahkumar@users.noreply.github.com>
2020-08-13 20:05:40 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -1,6 +1,7 @@
 
-
-import os
+from userbot import * ;  from sys import * ; from telethon import TelegramClient, functions, types ; from telethon.tl.types import InputMessagesFilterDocument ; from pathlib import Path; from userbot.javes_main.commands import * ; import asyncio, os, traceback, sys, traceback, os, importlib, glob ; javes = tgbot = bot.tgbot = client 
+from telethon.tl.types import InputMessagesFilterDocument
+from importlib import import_module
 from sys import version_info
 from logging import basicConfig, getLogger, INFO, DEBUG, WARNING
 from distutils.util import strtobool as sb
@@ -119,17 +120,55 @@ if BOT_TOKEN:
 
 owner = owner2 = owner3 = owner4 = None
 
-async def check_owners():
-      owner = await client.get_me()
-      if client2:
-          owner2 = await client2.get_me()
-      if client3:
-          owner3 = await client3.get_me()
-      if tebot:
-          owner4 = await tebot.get_me()
-with client:
-        client.loop.run_until_complete(check_owners())
-    
+
+
+
+#####################################
+plugin_channel = "@pldhsys"  #this is official plugin channel for javes 
+#####################################
+
+
+async def a():
+  LOGS.info("Connecting...") ; 
+  o = o2 = o3 = o4 = ""
+  la = 0
+  try:
+     await client.start() ; LOGS.info("client connected") ; o = "Client1" ; owner = await client.get_me()
+  except:
+    LOGS.info("Telegram String Session Wrong or Expired Please Add new one ") ; quit(1)
+  if client2:
+      try:
+        await client2.start() ; LOGS.info("client2 connected") ; o2 = ", Client2" ; owner2 = await client2.get_me()
+      except:
+         LOGS.info("client2 Session string Wrong/Expired Please add new string session or delete var S2") ; quit(1)
+  if client3:
+      try:
+         await client3.start() ; LOGS.info("client3 connected") ; o3 = ", Client3" ; owner3 = await client3.get_me()
+      except:
+         LOGS.info("client3 Session string Wrong/Expired Please add new string  or delete var S3 ") ; quit(1)
+  if tebot:
+      try:
+         await tebot.start() ; LOGS.info("Telegram Bot connected") ; o4 = ", TGBot" ; owner4 = await tebot.get_me()
+      except:
+         LOGS.info("Bot Token Wrong/ Expired please add new one  or delete var BOT_TOKEN ") ; quit(1)
+  test1 = await client.get_messages(plugin_channel, None , filter=InputMessagesFilterDocument) ; total = int(test1.total) ; total_doxx = range(0, total)
+  for ixo in total_doxx:
+       mxo = test1[ixo].id ; await client.download_media(await client.get_messages(cIient, ids=mxo), "userbot/modules/")
+  ar = glob.glob("userbot/modules/*.py")
+  f = len(ar)
+  LOGS.info(f" loading {f} modules it may take 1 minute please wait")
+  for i in ar:
+     br = os.path.basename(i)
+     cr = (os.path.splitext(br)[0])
+     import_module(f"userbot.modules.{cr}")
+     la += 1
+     LOGS.info(f" loaded {la}/{f} modules")  
+  os.system("rm userbot/modules/*.py") ; LOGS.info(f"Sucessfully connected with {o}{o2}{o3}{o4} check it by typing !javes in any client's chat, type  !help for more info.")
+  if len(argv) not in (1, 3, 4):
+       await javes.disconnect()
+  else:
+       await javes.run_until_disconnected()
+       
 
 
 
